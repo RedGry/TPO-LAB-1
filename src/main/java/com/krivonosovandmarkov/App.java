431e6467946f63fbcebd3d903205fe651b85568e
pdf_markov <pdf.markov@gmail.com>
2022-02-20 18:27:38 +0300

    delete main logic

--- src/main/java/com/krivonosovandmarkov/App.java
+++ src/main/java/com/krivonosovandmarkov/App.java
@@ -1,12 +1,14 @@
 package com.krivonosovandmarkov;
 
 import com.krivonosovandmarkov.task1.Sin;
+import com.krivonosovandmarkov.task2.HeapSort;
+
+import java.util.Arrays;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println(Sin.calc( -2.14, 100));
-        System.out.println(Math.sin(-2.14));
+
     }
 }
